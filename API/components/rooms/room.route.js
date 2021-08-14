ef647f797d2d838c3ff046a138dc2660718d2e77
Huy Nguyen <huy.nguyen2@bnksolution.com>
2021-08-14 09:58:05 +0700

    update code BE

--- API/components/rooms/room.route.js
+++ API/components/rooms/room.route.js
@@ -5,9 +5,8 @@ const RoomChatModel = require("../rooms/room.model");
 router.get("/getRoomChatById", async (req, res) => {
   try {
     let { userId, partnerId } = req.query;
-    let getData = await RoomChatModel.findOne({ users: { $in: [userId, partnerId] } }).populate("message.users");
+    let getData = await RoomChatModel.findOne({ users: partnerId }).populate("message.users");
     // let getData = await RoomChatModel.find({ users: { $or: [userId, partnerId] } }).populate("message.users");
-    console.log("🚀 ~ file: room.route.js ~ line 9 ~ router.get ~ getData", getData)
 
     if (getData) {
       res.json({
